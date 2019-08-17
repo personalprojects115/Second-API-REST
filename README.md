fccd0366b7c88d01262b41bcda10f096e9f85d11
Senac Informática <aluno@senacinformatica.br>
2019-08-17 17:22:38 -0300

    Terminando projeto

---
+++ README.md
@@ -0,0 +1,6 @@
+# Angular 
+
+## Second Workshop ft. Angular
+
+Second workshop about Angular framework. This time, building the front-end of the application
+
--- WorkshopAngularII/src/app/app.module.ts
+++ WorkshopAngularII/src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
